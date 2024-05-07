924cbcf54563cf3b2abdf04e8b3bc3e3463cc96c
unknown <mangulabnanshervinmarco@gmail.com>
2024-05-07 14:25:40 +0700

    added pseudocode

---
+++ simple_calculator.py
@@ -0,0 +1,6 @@
+# Pseudocode
+# Print the operations to choose from
+# Get user's choice for the math operation
+# Get user's input for two numbers
+# Perform the selected math operation and display the result
+# Ask the user if they want to try again
