8cddbf08489e6c67d6fc7686489e5f467871ca9e
unknown <mangulabnanshervinmarco@gmail.com>
2024-05-07 14:40:01 +0700

    get user's choice for the math operation

--- simple_calculator.py
+++ simple_calculator.py
@@ -9,6 +9,9 @@ print("3. Multiplication")
 print("4. Division")
 print("5. Exit")
 # Get user's choice for the math operation
+choice = int(input("\nEnter choice (1-5): "))
+if choice == 5:
+    print("Thank you!")
 # Get user's input for two numbers
 # Perform the selected math operation and display the result
 # Ask the user if they want to try again
